04fb5767d9503ce8e4b3be5da7c45e20148d061c
Jason Gutierrez <jasong84@gmail.com>
2020-07-16 14:50:43 -0500

    Save first C# Edabit challenge

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EdaBitChallenges
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello Edabit Challenges!");
+			var test = "Happy Birthday";
+			var result = StringChallenges.ReverseCase(test);
+			Console.WriteLine(result);
+
+		}
+	}
+}
---
+++ StringChallenges.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace EdaBitChallenges
+{
+	public static class StringChallenges
+	{
+		public static string ReverseCase(string str)
+		{
+			return string.Concat(str
+				.ToCharArray()
+				.Select(c => char.IsLower(c) ? c.ToString().ToUpper() : c.ToString().ToLower())
+				.ToArray());
+		}
+	}
+}

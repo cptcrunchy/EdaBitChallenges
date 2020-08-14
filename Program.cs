3e3d4fa0cfd26882ffe6e2cb06954c783e59bbaf
Jason Gutierrez <jasong84@gmail.com>
2020-08-14 17:00:52 -0500

    Add solution for RollingCipher

--- Program.cs
+++ Program.cs
@@ -6,11 +6,7 @@ namespace EdaBitChallenges
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello Edabit Challenges!");
-			var test = "Happy Birthday";
-			var result = StringChallenges.ReverseCase(test);
-			Console.WriteLine(result);
-
+		
 		}
 	}
 }
---
+++ RollingCipher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace EdaBitChallenges
+{
+	public class RollingCipher
+	{
+		public string Cipher(string str, int n)
+		{
+			return string.Join("", str.ToCharArray().Select(c => {
+				var d = c < 'a' ? 65 : 97;
+				var diff = n < 0 ? (26 + n) : n;
+				return char.ConvertFromUtf32((c - d + diff) % 26 + d);				
+			}).ToArray());
+		}
+	}
+}
